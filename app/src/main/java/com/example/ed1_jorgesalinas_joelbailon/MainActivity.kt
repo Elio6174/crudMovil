b72ba430787adc3e2cdb01b0de09d7e8e12a8c0c
Jorge Salinas <srjo230596@upemor.edu.mx>
2026-06-15 18:55:48 -0600

    enlace de menu a activityMenuCalculadora

--- app/src/main/java/com/example/ed1_jorgesalinas_joelbailon/MainActivity.kt
+++ app/src/main/java/com/example/ed1_jorgesalinas_joelbailon/MainActivity.kt
@@ -1,20 +1,21 @@
 package com.example.ed1_jorgesalinas_joelbailon
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val botonCalculadora = findViewById<Button>(R.id.btnMenuCalculadora)
+
+        botonCalculadora.setOnClickListener {
+            val intent = Intent(this, menuCalculadora::class.java)
+            startActivity(intent)
         }
     }
 }
